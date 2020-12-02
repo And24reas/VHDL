d94ef980c239025b21276e5586c9248f4866eada
andreas <meinakkount@gmail.com>
2020-12-02 11:31:00 +0100

    developed counters added State Machine testbench

--- Edge_Detector/counters.vhd
+++ Edge_Detector/counters.vhd
@@ -49,6 +49,7 @@ end Counters;
 
 architecture Behavioral of Counters is
 Signal clk_cycles , HoursInt, MinutesInt, SecondsInt: integer;
+Signal  q1,q2,edge:std_logic;
 begin
     process(Clk) is
         begin
@@ -58,7 +59,7 @@ begin
                     Seconds <= "000000";
                     Minutes <= "000000";
                     Hours <= "00000";
-                else
+                 else
                     if clk_cycles = (Fin-1) then
                         clk_cycles <= 0;
                          if SecondsInt = 59 then
@@ -88,6 +89,74 @@ begin
                     end if;
                 end if;
             else
+                if(clk'event and clk='1') then
+                    if(Reset='0') then
+                        if (Up='1' or Down='1') then
+                            q1 <= '1';
+                    else
+                        q1 <= '0';
+                    end if;
+                end if;
+                q2 <= q1;
+                edge <= not q2 and q1;
+                if(edge='1' and Up='1') then
+                    if(Condition(1)= '1') then
+                        if(Condition(0)='0') then
+                            if(MinutesInt = 59) then
+                                MinutesInt <= 0;
+                                Minutes <= std_logic_vector(to_unsigned(MinutesInt,6));
+                            else
+                                MinutesInt <= MinutesInt +1; 
+                                Minutes <= std_logic_vector(to_unsigned(MinutesInt,6));
+                            end if;
+                        else
+                            if(SecondsInt = 59) then
+                                SecondsInt <= 0;
+                                Seconds <= std_logic_vector(to_unsigned(SecondsInt,6));
+                            else
+                                SecondsInt <= SecondsInt +1; 
+                                Seconds <= std_logic_vector(to_unsigned(SecondsInt,6));
+                            end if;
+                        end if;
+                     else 
+                        if(HoursInt = 23) then
+                            HoursInt <= 0;
+                            Hours <= std_logic_vector(to_unsigned(HoursInt,5));
+                        else                     
+                            HoursInt <= HoursInt +1; 
+                            Hours <= std_logic_vector(to_unsigned(HoursInt,5));
+                        end if;
+                     end if;
+                 elsif(edge='1' and Down = '1') then 
+                    if(Condition(1)= '1') then
+                        if(Condition(0)='0') then
+                            if(MinutesInt = 0) then
+                                MinutesInt <= 59;
+                                Minutes <= std_logic_vector(to_unsigned(MinutesInt,6));
+                            else
+                                MinutesInt <= MinutesInt - 1; 
+                                Minutes <= std_logic_vector(to_unsigned(MinutesInt,6));
+                            end if;
+                        else
+                            if(SecondsInt = 0) then
+                                SecondsInt <= 59;
+                                Seconds <= std_logic_vector(to_unsigned(SecondsInt,6));
+                            else
+                                SecondsInt <= SecondsInt - 1; 
+                                Seconds <= std_logic_vector(to_unsigned(SecondsInt,6));
+                            end if;
+                        end if;
+                     else 
+                        if(HoursInt = 0) then
+                            HoursInt <= 23;
+                            Hours <= std_logic_vector(to_unsigned(HoursInt,5));
+                        else                     
+                            HoursInt <= HoursInt - 1; 
+                            Hours <= std_logic_vector(to_unsigned(HoursInt,5));
+                        end if;
+                     end if;
+                end if;         
             end if;
+        end if;
         end process;
 end Behavioral;
